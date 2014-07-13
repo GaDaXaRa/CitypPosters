5f3eb18663096142c73f851273ed69062c0481fa
Miguel Santiago <gadaxara@gmail.com>
2014-07-13 16:15:40 +0200

    Initial Commit

---
+++ CitypPosters/CYPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CYPAppDelegate.h
+//  CitypPosters
+//
+//  Created by Miguel Santiago Rodríguez on 13/07/14.
+//  Copyright (c) 2014 gadaxara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CitypPosters/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CitypPosters
+//
+//  Created by Miguel Santiago Rodríguez on 13/07/14.
+//  Copyright (c) 2014 gadaxara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CYPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CYPAppDelegate class]));
+    }
+}
